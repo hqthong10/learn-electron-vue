456d6158cdf8ce4c4ae4ad6ca95b07cba4b60177
Quang Thông <hq.thong10@gmail.com>
2025-05-11 20:08:41 +0700

    u

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '../views/home/index.vue';
 
-Vue.use(Router);
+// Vue.use(Router);
 
 export default new Router({
   mode: 'hash', // ⚠️ Quan trọng cho Electron: dùng 'hash' để tránh lỗi đường dẫn
